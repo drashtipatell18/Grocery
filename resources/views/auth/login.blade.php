4004631148b7852fe9b8c3c42242061be06374bb
drashtipatell18 <drashti.kalathiyainfotech@gmail.com>
2024-07-12 16:40:24 +0530

    add login code

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,18 @@
 @extends('layouts.app')
+<head>
+    <style>
+        /* .sidebar .nav .nav-item.active > .nav-link .menu-title{
+          color: #6ad1a7 !important;
+        }
+        .sidebar .nav .nav-item.active > .nav-link i{
+          color: #6ad1a7 !important;
+        } */
+        .brand-logo {
+          height: 50px !important;
+          text-align: center;
+        }
+      </style>
+</head>
 @section('content')
     <div class="container-fluid page-body-wrapper full-page-wrapper">
         <div class="content-wrapper d-flex align-items-center auth">
@@ -6,7 +20,7 @@
                 <div class="col-lg-4 mx-auto">
                     <div class="auth-form-light text-left p-5">
                         <div class="brand-logo">
-                            <img src="../../assets/images/logo.svg">
+                            <img src="{{ asset('assets/images/logo.png')}}">
                         </div>
                         <h6 class="font-weight-light">Login</h6>
                         <form class="pt-3">
